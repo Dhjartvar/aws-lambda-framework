4db422acbc0b4732b0822b8ab6046e11baaf3cc8
Dan Olsen <do@intelligentbanker.com>
2019-12-12 17:49:56 +0100

    Feat: APIGatewayResponse and HttpStatusCode for formatting Lambda responses to API Gateway

---
+++ src/response/APIGatewayResponse.ts
@@ -0,0 +1,19 @@
+import { APIGatewayProxyResult } from 'aws-lambda'
+import { HttpStatusCode } from './HttpStatusCode'
+
+export default abstract class APIGatewayResponse {
+  static build(statusCode: HttpStatusCode, message: string | object) {
+    let response: APIGatewayProxyResult = {
+      statusCode: statusCode,
+      headers: {
+        'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+        'Access-Control-Allow-Credentials': true, // Required for cookies, authorization headers with HTTPS
+        'content-type': 'application/json'
+      },
+      body: typeof message === 'string' ? message : JSON.stringify(message),
+      isBase64Encoded: false
+    }
+
+    return response
+  }
+}
---
+++ src/response/HttpStatusCode.ts
@@ -0,0 +1,8 @@
+export enum HttpStatusCode {
+  Ok = 200,
+  BadRequest = 400,
+  Unauthorized = 401,
+  Forbidden = 403,
+  NotFound = 404,
+  InternalServerError = 500
+}
