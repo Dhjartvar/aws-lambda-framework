132a4c5ac4ed863334e643cd60e3041191411d4f
Dan Olsen <do@intelligentbanker.com>
2019-12-13 17:19:11 +0100

    Refactor: Moved SlackNotifier to services

--- src/services/SlackNotifier.ts
+++ src/services/SlackNotifier.ts
@@ -3,7 +3,7 @@ import BaseNotifier from 'lambda-slack-notifier'
 import { Context } from 'aws-lambda'
 import { SlackNotificationColor } from 'lambda-slack-notifier/dist/SlackNotificationColor'
 import { IncomingWebhookResult } from '@slack/webhook'
-import { Environment } from 'src/storage/Environment'
+import { Environment } from 'src/container/Environment'
 
 @Service()
 export default class SlackNotifier extends BaseNotifier {
@@ -13,11 +13,9 @@ export default class SlackNotifier extends BaseNotifier {
 
   /**
    * Override notify function to only notify when in production
-   * @param message
-   * @param color
    */
   notify(message: unknown, color?: SlackNotificationColor | string): Promise<IncomingWebhookResult | void> {
-    if (Container.get('environment') !== Environment.Production) return super.notify(String(message), color)
-    else return Promise.resolve()
+    if (Container.get('environment') !== Environment.Production || !this.webhookUrl) return Promise.resolve()
+    else return super.notify(String(message), color)
   }
 }
