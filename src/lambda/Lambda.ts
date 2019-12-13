d8d2acf984b11982550058d5edbb5f913600fbcd
Dan Olsen <do@intelligentbanker.com>
2019-12-13 17:20:38 +0100

    Refactor: Updated Lambda to reflect structural and syntax changes

--- src/lambda/Lambda.ts
+++ src/lambda/Lambda.ts
@@ -2,8 +2,9 @@ import { Context, APIGatewayProxyResult, APIGatewayProxyEvent } from 'aws-lambda
 import { HttpStatusCode } from '../response/HttpStatusCode'
 import APIGatewayResponse from '../response/APIGatewayResponse'
 import { Container } from 'typedi'
-import SlackNotifier from 'src/notification/SlackNotifier'
-import { StoreToken } from 'src/storage/StoreToken'
+import SlackNotifier from 'src/services/SlackNotifier'
+import { DatabaseToken } from 'src/container/DatabaseToken'
+import { Environment } from 'src/container/Environment'
 
 export default abstract class Lambda {
   readonly event: APIGatewayProxyEvent
@@ -27,9 +28,10 @@ export default abstract class Lambda {
     Container.set('query-string-parameters', this.queryStringParameters)
     Container.set('path-parameters', this.pathParameters)
     Container.set('context', this.pathParameters)
-    Container.set('environment', process.env.NODE_ENV)
+    Container.set('environment', process.env.NODE_ENV ?? Environment.Development)
     Container.set('slack-webhook', process.env.SLACK_WEBHOOK)
     Container.set('pooling', process.env.pooling ?? true)
+    Container.set('logging', process.env.logging ?? false)
   }
 
   abstract async invoke(): Promise<any>
@@ -42,7 +44,7 @@ export default abstract class Lambda {
       Container.get(SlackNotifier).notify(e)
       return APIGatewayResponse.build(e.statusCode || HttpStatusCode.InternalServerError, e.errorMessage || e)
     } finally {
-      Container.getMany(StoreToken).forEach(store => store.end())
+      await Promise.all(Container.getMany(DatabaseToken).map(database => database.end()))
     }
   }
 
@@ -58,13 +60,15 @@ export default abstract class Lambda {
     return this
   }
 
-  public setPooling(boolean: boolean) {
-    Container.set('pooling', boolean)
+  public setPooling(enabled: boolean) {
+    Container.set('pooling', enabled)
 
     return this
   }
 
-  public validateInput() {
-    throw 'Not yet implemented'
+  public setLogging(enabled: boolean) {
+    Container.set('pooling', enabled)
+
+    return this
   }
 }
