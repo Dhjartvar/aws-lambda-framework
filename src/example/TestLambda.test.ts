be83740f87bacd12aa4674d004fbaa97e4288046
Dan Olsen <do@intelligentbanker.com>
2019-12-17 17:56:14 +0100

    Feat: Test for TestLambda handler

--- src/example/TestLambda.test.ts
+++ src/example/TestLambda.test.ts
@@ -2,37 +2,24 @@ import { TestLambda } from './TestLambda'
 import { testEvent } from '../test/LambdaTestEvent'
 import { testContext } from '../test/LambdaTestContext'
 import Container from 'typedi'
-import { Environment } from '../container/Environment'
-import Aurora from '../services/Aurora'
-
+import { Environment } from '../framework/enums/Environment'
 let testLambda: TestLambda
 
 describe('TestLambda', () => {
   beforeAll(() => {
-    testLambda = new TestLambda(testEvent, testContext)
-      .setEnvironment(Environment.Development)
-      .setPooling(false)
-      .setSlackWebhook('test')
-  })
-
-  it('should have set pooling to false', async () => {
-    expect(Container.get('pooling')).toBeFalsy()
+    testLambda = new TestLambda(testEvent, testContext).setEnvironment(Environment.Development)
   })
 
   it('should have slack-webhook set to test', async () => {
-    expect(Container.get('slack-webhook')).toBe('test')
+    expect(Container.get('slack-webhook')).toBe(process.env.SLACK_WEBHOOK)
   })
 
   it('should have environment set to dev', async () => {
-    expect(Container.get('environment')).toBe('dev')
+    expect(Container.get('environment')).toBe(Environment.Development)
   })
 
-  it('WIP', async () => {
-    try {
-      let res = await testLambda.handler()
-      console.log('RES: ', res)
-    } catch (err) {
-      console.error(err)
-    }
+  it('should call the handler and get a response back with status code 200', async () => {
+    let res = await testLambda.handler()
+    expect(res.statusCode).toBe(200)
   })
 })
