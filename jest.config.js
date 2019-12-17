63f08e71a84515461a1167a0067aa55b04bf5c39
Dan Olsen <do@intelligentbanker.com>
2019-12-17 17:51:18 +0100

    Refactor: Unnecessary file ending specification

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 const { pathsToModuleNameMapper } = require('ts-jest/utils')
-const { compilerOptions } = require('./tsconfig.json')
+const { compilerOptions } = require('./tsconfig')
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
