4a7f4a6ebde5dc0a1b6ab08f77c304f720886524
Dan Olsen <do@intelligentbanker.com>
2019-12-18 17:15:16 +0100

    Fix: Removed dependency from interface

--- src/framework/interfaces/Notifier.ts
+++ src/framework/interfaces/Notifier.ts
@@ -1,5 +1,3 @@
-import { SlackNotificationColor } from 'lambda-slack-notifier/dist/SlackNotificationColor'
-
 export default interface Notifier {
-  notify(message: any, notificationColor: string | SlackNotificationColor): void
+  notify(message: any, notificationColor?: string): void
 }
