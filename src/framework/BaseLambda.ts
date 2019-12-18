6d24f2043bb1b254862616793d7c628d8ca5a59b
Dan Olsen <do@intelligentbanker.com>
2019-12-18 17:16:24 +0100

    Refactor: BaseLambda now uses inversify instead of typedi. Simplified code and integrated APIGatewayResponse as private method.
    The APIGatewayResponse class would ever only be used here, so it makes sense to have it as a private function instead.

--- src/framework/BaseLambda.ts
+++ src/framework/BaseLambda.ts
@@ -1,73 +1,54 @@
 import { Context, APIGatewayProxyResult, APIGatewayProxyEvent } from 'aws-lambda'
-import { HttpStatusCode } from './enums/HttpStatusCode'
-import APIGatewayResponse from './response/APIGatewayResponse'
-import { Container } from 'typedi'
+import { HttpStatusCode } from '@enums/HttpStatusCode'
+import { Environment } from '@enums/Environment'
+import LambdaContainer from '@framework/LambdaContainer'
+import LambdaFunction from '@interfaces/LambdaFunction'
 import SlackNotifier from '@services/SlackNotifier'
-import { Environment } from './enums/Environment'
+import { Property } from '@framework/symbols/Property'
 import Aurora from '@services/Aurora'
 import Redshift from '@services/Redshift'
-import { RegionName } from 'aws-sdk/clients/dynamodb'
-import { Region } from './enums/Region'
-import AuroraConfig from './interfaces/AuroraConfig'
-import { PoolOptions, ConnectionOptions } from 'mysql2'
 require('dotenv').config()
 
-export default abstract class BaseLambda {
-  readonly event: APIGatewayProxyEvent
-  readonly body?: object
-  readonly queryStringParameters?: any
-  readonly pathParameters?: any
-  readonly context: Context
-
+export default abstract class BaseLambda implements LambdaFunction {
   constructor(event: APIGatewayProxyEvent, context: Context) {
-    this.event = typeof event === 'string' ? JSON.parse(event) : event
-    this.body = typeof this.event.body === 'string' ? JSON.parse(this.event.body) : this.event.body
-    this.queryStringParameters = this.event.queryStringParameters
-    this.pathParameters = this.event.pathParameters
-    this.context = context
-    this.initializeContainer()
-  }
-
-  private initializeContainer(): void {
-    Container.set('body', this.body)
-    Container.set('event', this.event)
-    Container.set('query-string-parameters', this.queryStringParameters)
-    Container.set('path-parameters', this.pathParameters)
-    Container.set('context', this.context)
-    Container.set('region', process.env.REGION ?? 'eu-central-1')
-    Container.set('environment', process.env.NODE_ENV ?? Environment.Development)
-    Container.set('logging', process.env.LOGGING ?? false)
+    LambdaContainer.bind<APIGatewayProxyEvent>(Property.EVENT).toConstantValue(event)
+    LambdaContainer.bind<Context>(Property.CONTEXT).toConstantValue(context)
+    let eventBody = typeof event.body === 'string' ? JSON.parse(event.body) : event.body
+    LambdaContainer.bind<object>(Property.EVENT_BODY).toConstantValue(eventBody)
+    LambdaContainer.bind<string>(Property.REGION).toConstantValue(process.env.REGION ?? 'eu-central-1')
+    LambdaContainer.bind<string>(Property.ENVIRONMENT).toConstantValue(process.env.NODE_ENV ?? Environment.Development)
+    LambdaContainer.bind<boolean>(Property.LOGGING).toConstantValue(
+      process.env.LOGGING ? JSON.parse(process.env.LOGGING) : false
+    )
   }
 
   abstract async invoke(): Promise<any>
 
   async handler(): Promise<APIGatewayProxyResult> {
     try {
-      return APIGatewayResponse.build(HttpStatusCode.Ok, await this.invoke())
+      return this.APIGatewayResponse(HttpStatusCode.Ok, await this.invoke())
     } catch (e) {
       console.error(e)
-      await Container.get(SlackNotifier).notify(e.message ?? e)
-      return APIGatewayResponse.build(e.statusCode ?? HttpStatusCode.InternalServerError, e.message ?? e)
+      await LambdaContainer.get(SlackNotifier).notify(e.message ?? e)
+      return this.APIGatewayResponse(e.statusCode ?? HttpStatusCode.InternalServerError, e.message ?? e)
     } finally {
-      await Promise.all([Container.get(Aurora).end(), Container.get(Redshift).end()])
+      if (LambdaContainer.isBound(Aurora)) await LambdaContainer.get(Aurora).end()
+      if (LambdaContainer.isBound(Redshift)) await LambdaContainer.get(Redshift).end()
     }
   }
 
-  setEnvironment(environment: Environment) {
-    Container.set('environment', environment)
+  private APIGatewayResponse(statusCode: HttpStatusCode, message: string | object) {
+    let response: APIGatewayProxyResult = {
+      statusCode: statusCode,
+      headers: {
+        'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+        'Access-Control-Allow-Credentials': true, // Required for cookies, authorization headers with HTTPS
+        'content-type': 'application/json'
+      },
+      body: typeof message === 'string' ? message : JSON.stringify(message),
+      isBase64Encoded: false
+    }
 
-    return this
-  }
-
-  setRegion(region: Region) {
-    Container.set('region', region)
-
-    return this
-  }
-
-  setLogging(enabled: boolean) {
-    Container.set('logging', enabled)
-
-    return this
+    return response
   }
 }
