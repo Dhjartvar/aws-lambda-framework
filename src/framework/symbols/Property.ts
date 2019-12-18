b767fef858b922f8d98631abf082104faa4fce96
Dan Olsen <do@intelligentbanker.com>
2019-12-18 17:18:09 +0100

    Feat: add inversify symbols for services and properties

---
+++ src/framework/symbols/Property.ts
@@ -0,0 +1,8 @@
+export const Property = {
+  EVENT: Symbol.for('Event'),
+  EVENT_BODY: Symbol.for('EventBody'),
+  CONTEXT: Symbol.for('Context'),
+  REGION: Symbol.for('Region'),
+  ENVIRONMENT: Symbol.for('Environment'),
+  LOGGING: Symbol.for('Logging')
+}
---
+++ src/framework/symbols/Service.ts
@@ -0,0 +1,9 @@
+export const Service = {
+  CONNECTION: Symbol.for('Connection'),
+  VALIDATOR: Symbol.for('Validator'),
+  NOTIFIER: Symbol.for('Notifier'),
+  LAMBDA: Symbol.for('Lambda'),
+  DYNAMODB: Symbol.for('DynamoDB'),
+  S3: Symbol.for('S3'),
+  CONTEXT: Symbol.for('Context')
+}
