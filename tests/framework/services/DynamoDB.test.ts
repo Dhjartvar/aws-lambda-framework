ce50aeec52bb0bf07956fb70b7dd19f51a0e6fb6
Dan Olsen <do@intelligentbanker.com>
2019-12-19 15:43:18 +0100

    Feat: DynamoDB test

---
+++ tests/framework/services/DynamoDB.test.ts
@@ -0,0 +1,16 @@
+import DynamoDB from '../../../src/framework/services/DynamoDB'
+import LambdaContainer from '../../../src/framework/LambdaContainer'
+
+describe('DynamoDB', () => {
+  beforeAll(() => {
+    if (!process.env.DYNAMODB_TEST_TABLE) throw 'Missing env var DYNAMODB_TEST_TABLE for DynamoDB.test.ts'
+  })
+
+  it('should scan a DynamoDB table and retrieve more than zero rows', async () => {
+    let res = await LambdaContainer.get(DynamoDB)
+      .scan({ TableName: process.env.DYNAMODB_TEST_TABLE! })
+      .promise()
+
+    expect(res.Count).toBeGreaterThan(0)
+  })
+})
