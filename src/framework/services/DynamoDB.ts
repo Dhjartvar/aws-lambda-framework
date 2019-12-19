e94b0526fc0a429a0da750364e381f348db4d022
Dan Olsen <do@intelligentbanker.com>
2019-12-19 15:39:51 +0100

    Refactor: Region is now set globally in LambdaContainer. DynamoDB now extends the DynamoDB DocumentClient.

--- src/framework/services/DynamoDB.ts
+++ src/framework/services/DynamoDB.ts
@@ -1,11 +1,5 @@
-import { DynamoDB as AWSDynamoDB } from 'aws-sdk'
-import { injectable, inject } from 'inversify'
-import { Property } from '@framework/symbols/Property'
+import AWS from 'aws-sdk'
+import { injectable } from 'inversify'
 
 @injectable()
-export default class DynamoDB extends AWSDynamoDB {
-  constructor(@inject(Property.REGION) region: string) {
-    if (!region) throw 'Missing config for Lambda: region. Region can be set using process.env.REGION'
-    super({ region: region })
-  }
-}
+export default class DynamoDB extends AWS.DynamoDB.DocumentClient {}
