3a9225de9cc71362921899b1e4dea4d5f0176039
Dan Olsen <do@intelligentbanker.com>
2019-12-19 15:41:55 +0100

    Refactor: Region is now set globally. Altered imports slightly

--- src/framework/services/S3.ts
+++ src/framework/services/S3.ts
@@ -1,11 +1,5 @@
-import { S3 as AWSS3 } from 'aws-sdk'
-import { injectable, inject } from 'inversify'
-import { Property } from '../symbols/Property'
+import AWS from 'aws-sdk'
+import { injectable } from 'inversify'
 
 @injectable()
-export default class S3 extends AWSS3 {
-  constructor(@inject(Property.REGION) region: string) {
-    if (!region) throw 'Missing config for Lambda: region. Region can be set using process.env.REGION'
-    super({ region: region })
-  }
-}
+export default class S3 extends AWS.S3 {}
