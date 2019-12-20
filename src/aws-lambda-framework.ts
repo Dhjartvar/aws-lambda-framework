29c730ca3f6e655c0776f2cbe5e13d54a375bb81
Dan Olsen <do@intelligentbanker.com>
2019-12-20 14:01:19 +0100

    Feat: add enums to exports

--- src/aws-lambda-framework.ts
+++ src/aws-lambda-framework.ts
@@ -1,5 +1,8 @@
 import LambdaContainer from '@framework/LambdaContainer'
 import BaseLambda from '@framework/BaseLambda'
+import { Environment } from '@framework/enums/Environment'
+import { Region } from '@framework/enums/Region'
+import { HttpStatusCode } from '@framework/enums/HttpStatusCode'
 import Aurora from '@framework/services/Aurora'
 import Redshift from '@framework/services/Redshift'
 import InputValidator from '@framework/services/InputValidator'
@@ -9,4 +12,18 @@ import DynamoDB from '@framework/services/DynamoDB'
 import Lambda from '@framework/services/Lambda'
 import { Property } from '@framework/symbols/Property'
 
-export { LambdaContainer, BaseLambda, Aurora, Redshift, InputValidator, SlackNotifier, S3, DynamoDB, Lambda, Property }
+export {
+  LambdaContainer,
+  BaseLambda,
+  Environment,
+  Region,
+  HttpStatusCode,
+  Aurora,
+  Redshift,
+  InputValidator,
+  SlackNotifier,
+  S3,
+  DynamoDB,
+  Lambda,
+  Property
+}
