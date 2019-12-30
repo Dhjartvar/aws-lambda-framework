16bc20adeef0a8ec135ec56a22a10bd108857529
Dan Olsen <do@intelligentbanker.com>
2019-12-30 17:15:36 +0100

    Refactor: removed error constants in favour of Error objects

--- src/framework/services/InputValidator.ts
+++ src/framework/services/InputValidator.ts
@@ -2,30 +2,27 @@ import { isRight } from 'fp-ts/lib/Either'
 import { Type } from 'io-ts'
 import { injectable } from 'inversify'
 import Validator from '@framework/interfaces/Validator'
-import { INVALID_INPUT_ERROR } from '@framework/constants/Errors'
 
 @injectable()
-export default class InputValidator implements Validator {
+export class InputValidator implements Validator {
   /**
    * Validate objects or JSON strings with io-ts and return them if valid
    * @returns the validated input
    */
-  validate<ReturnType, Validator extends Type<ReturnType>>(input: object, type: Validator): ReturnType {
-    if (!input) throw INVALID_INPUT_ERROR(input, type)
+  validate<ReturnType, Validator extends Type<ReturnType>>(input: object | string, type: Validator): ReturnType {
     if (typeof input === 'string') input = this.parseJson(input)
 
     let validationResult = type.decode(input)
 
     if (isRight(validationResult)) return validationResult.right
-    else throw INVALID_INPUT_ERROR(input, type)
+    else throw Error(`Received invalid input ${JSON.stringify(input)} expected to receive type ${type.name}`)
   }
 
   private parseJson(input: string): object {
     try {
       return JSON.parse(input)
-    } catch (e) {
-      console.warn(e)
-      throw e
+    } catch (err) {
+      throw Error(err)
     }
   }
 }
