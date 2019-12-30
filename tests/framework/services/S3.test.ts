b3462464f688327e4c2fd522b060523169d92c83
Dan Olsen <do@intelligentbanker.com>
2019-12-30 17:10:19 +0100

    Refactor: imports and finished basic test

--- tests/framework/services/S3.test.ts
+++ tests/framework/services/S3.test.ts
@@ -1,12 +1,7 @@
-import S3 from '../../../src/framework/services/S3'
-import LambdaContainer from '../../../src/framework/LambdaContainer'
+import { LambdaContainer, S3 } from '../../../src/aws-lambda-framework'
 
 describe('S3', () => {
-  it('should list buckets from S3 and retrieve more than zero buckets', async () => {
-    let res = await LambdaContainer.get(S3)
-      .listBuckets()
-      .promise()
-
-    console.log('RES: ', res)
+  it('should resolve when trying to list buckets ', async () => {
+    await expect(LambdaContainer.get(S3).listBuckets().promise()).resolves.toBeDefined()
   })
 })
