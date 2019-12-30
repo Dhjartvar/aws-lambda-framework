f233e4bc1fb214e645ff8e5e00407e486d29f507
Dan Olsen <do@intelligentbanker.com>
2019-12-30 17:11:28 +0100

    Feat: Tests to cover all code paths

--- tests/framework/services/InputValidator.test.ts
+++ tests/framework/services/InputValidator.test.ts
@@ -1,5 +1,4 @@
-import InputValidator from '../../../src/framework/services/InputValidator'
-import LambdaContainer from '../../../src/framework/LambdaContainer'
+import { LambdaContainer, InputValidator } from '../../../src/aws-lambda-framework'
 import { TestRequest, TestRequestType } from '../constants/TestRequest'
 
 describe('InputValidator', () => {
@@ -14,6 +13,30 @@ describe('InputValidator', () => {
     let validatedRequest: TestRequest = LambdaContainer.get(InputValidator).validate(request, TestRequestType)
 
     expect(validatedRequest).toBeDefined()
+  })
+
+  it('should validate JSON input correctly', () => {
+    let request: TestRequest = {
+      id: 123,
+      boolean: true,
+      number: 123,
+      string: 'test'
+    }
+
+    let validatedRequest: TestRequest = LambdaContainer.get(InputValidator).validate(
+      JSON.stringify(request),
+      TestRequestType
+    )
+
+    expect(validatedRequest).toBeDefined()
+  })
+
+  it('should throw an error because of input being a string instead of a stringified object', () => {
+    let request: string = 'a string instead of an object'
+
+    expect(() => {
+      let validatedRequest: TestRequest = LambdaContainer.get(InputValidator).validate(request, TestRequestType)
+    }).toThrow()
   })
 
   it('should fail because boolean is a string', () => {
