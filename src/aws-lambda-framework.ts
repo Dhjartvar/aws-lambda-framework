35355c1ce1595b6c8c9c463c8662cce9e33e6d1a
Dan Olsen <do@intelligentbanker.com>
2020-01-06 11:05:10 +0100

    Feat: now exports Context, Event and Result from aws-lambda to make Lambda imports cleaner

--- src/aws-lambda-framework.ts
+++ src/aws-lambda-framework.ts
@@ -1,4 +1,3 @@
-
 export { HttpStatusCode } from './framework/enums/HttpStatusCode'
 export { Environment } from './framework/enums/Environment'
 export { Property } from './framework/symbols/Property'
@@ -8,9 +7,9 @@ export { Redshift } from './framework/services/Redshift'
 export { InputValidator } from './framework/services/InputValidator'
 export { SlackNotifier } from './framework/services/SlackNotifier'
 export { DynamoDB } from './framework/services/DynamoDB'
+export { Context, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import AWS from 'aws-sdk'
 export const S3 = AWS.S3
 export const Lambda = AWS.Lambda
 export { BaseLambda } from './framework/BaseLambda'
 export { LambdaContainer } from './framework/LambdaContainer'
-
