e55045fb95f572b6e7d482d17013db45f9f599ce
Dan Olsen <do@intelligentbanker.com>
2020-01-07 15:47:16 +0100

    Feat: Service SSM (Systems Manager) added

--- src/framework/LambdaContainer.ts
+++ src/framework/LambdaContainer.ts
@@ -3,6 +3,7 @@ import 'reflect-metadata'
 import AWS from 'aws-sdk'
 const S3 = AWS.S3
 const Lambda = AWS.Lambda
+const SSM = AWS.SSM
 import { Property } from '@framework/symbols/Property'
 import { Environment } from '@framework/enums/Environment'
 import { Region } from '@framework/enums/Region'
@@ -22,7 +23,8 @@ LambdaContainer.bind<boolean>(Property.LOGGING).toConstantValue(
 
 AWS.config.update({ region: LambdaContainer.get(Property.REGION) })
 
-LambdaContainer.bind(Lambda).toConstantValue(new Lambda)
-LambdaContainer.bind(S3).toConstantValue(new S3)
+LambdaContainer.bind(Lambda).toConstantValue(new Lambda())
+LambdaContainer.bind(S3).toConstantValue(new S3())
+LambdaContainer.bind(SSM).toConstantValue(new SSM())
 
 export { LambdaContainer }
