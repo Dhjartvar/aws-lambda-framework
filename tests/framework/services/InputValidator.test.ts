a2ec38040201d669ee1c0ffc1868b31fdcd711eb
Dan Olsen <do@intelligentbanker.com>
2020-01-07 16:15:51 +0100

    Feat: updated integration tests for InputValidator

--- tests/framework/services/InputValidator.test.ts
+++ tests/framework/services/InputValidator.test.ts
@@ -3,10 +3,20 @@ import { TestInput } from '../constants/TestInput'
 
 describe('InputValidator', () => {
   it('should validate the input correctly', async () => {
-    let request = new TestInput('a valid string', 123)
+    let request = new TestInput('a valid string', 5)
 
-    let res = await LambdaContainer.get(InputValidator).validate(request)
+    await expect(LambdaContainer.get(InputValidator).validateOrReject(request)).resolves.toBeUndefined()
+  })
 
-    console.log('RES: ', res)
+  it('should reject because string is too short', async () => {
+    let request = new TestInput('too short', 5)
+
+    await expect(LambdaContainer.get(InputValidator).validateOrReject(request)).rejects.toBeDefined()
+  })
+
+  it('should reject because int is too high', async () => {
+    let request = new TestInput('long enough', 999)
+
+    await expect(LambdaContainer.get(InputValidator).validateOrReject(request)).rejects.toBeDefined()
   })
 })
