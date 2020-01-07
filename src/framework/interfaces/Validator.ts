d7209566f56cba6e7bd6cd0c9a0762c9fbdbde28
Dan Olsen <do@intelligentbanker.com>
2020-01-07 15:49:00 +0100

    Refactor: Replaced io-ts input validation with class-validator
    Class validator has a richer feature-set and cleaner code

--- src/framework/interfaces/Validator.ts
+++
@@ -1,5 +0,0 @@
-import { Type } from 'io-ts'
-
-export default interface Validator {
-  validate<ReturnType, Validator extends Type<ReturnType>>(input: object, type: Validator): ReturnType
-}
--- src/framework/services/InputValidator.ts
+++ src/framework/services/InputValidator.ts
@@ -1,28 +1,5 @@
-import { isRight } from 'fp-ts/lib/Either'
-import { Type } from 'io-ts'
+import { Validator } from 'class-validator/validation/Validator'
 import { injectable } from 'inversify'
-import Validator from '@framework/interfaces/Validator'
 
 @injectable()
-export class InputValidator implements Validator {
-  /**
-   * Validate objects or JSON strings with io-ts and return them if valid
-   * @returns the validated input
-   */
-  validate<ReturnType, Validator extends Type<ReturnType>>(input: object | string, type: Validator): ReturnType {
-    if (typeof input === 'string') input = this.parseJson(input)
-
-    let validationResult = type.decode(input)
-
-    if (isRight(validationResult)) return validationResult.right
-    else throw Error(`Received invalid input ${JSON.stringify(input)} expected to receive type ${type.name}`)
-  }
-
-  private parseJson(input: string): object {
-    try {
-      return JSON.parse(input)
-    } catch (err) {
-      throw Error(err)
-    }
-  }
-}
+export class InputValidator extends Validator {}
