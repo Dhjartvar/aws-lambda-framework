c89a5d0ef365380bb93003bf3276d58c537d6307
Dan Olsen <do@intelligentbanker.com>
2020-01-08 17:13:19 +0100

    Refactor: Imports. Can now take in stackTrace from prior error. Might be reworked

--- src/framework/errors/LambdaError.ts
+++ src/framework/errors/LambdaError.ts
@@ -1,5 +1,4 @@
-import { LambdaContainer, APIGatewayProxyEvent, Property, Context } from '../../aws-lambda-framework'
-import { HttpStatusCode } from '@framework/enums/HttpStatusCode'
+import { LambdaContainer, APIGatewayProxyEvent, Property, Context, HttpStatusCode } from '../../aws-lambda-framework'
 
 export class LambdaError extends Error {
   statusCode: number
@@ -10,11 +9,13 @@ export class LambdaError extends Error {
   userMessage: string
   constructor(
     errorMessage: string = `Lambda function ${LambdaContainer.get<Context>(Property.CONTEXT).functionName} failed.`,
+    stackTrace?: string,
     userMessage: string = `Failed to run ${LambdaContainer.get<Context>(Property.CONTEXT).functionName}!`,
     statusCode: number = HttpStatusCode.InternalServerError
   ) {
     super(errorMessage)
     this.errorMessage = errorMessage
+    if (stackTrace) this.stackTrace = stackTrace
     this.userMessage = userMessage
     this.statusCode = statusCode
   }
