0df910bed9b4a61d620a4532430e65dc23c7c474
Dan Olsen <do@intelligentbanker.com>
2020-01-08 17:18:10 +0100

    Chore: Removed unnecessary beforeAll statement

--- tests/framework/services/DynamoDB.test.ts
+++ tests/framework/services/DynamoDB.test.ts
@@ -1,10 +1,6 @@
 import { LambdaContainer, DynamoDB } from '../../../src/aws-lambda-framework'
 
 describe('DynamoDB', () => {
-  beforeAll(() => {
-    if (!process.env.DYNAMODB_TEST_TABLE) throw 'Missing env var DYNAMODB_TEST_TABLE for DynamoDB.test.ts'
-  })
-
   it('should resolve when trying to list tables', async () => {
     await expect(
       LambdaContainer.get(DynamoDB)
