f9248c3a96b8c80ce450a77840983dafb96fb511
Dan Olsen <do@intelligentbanker.com>
2020-01-08 17:13:38 +0100

    Refactor: Imports + stackTrace change

--- src/framework/errors/UnauthorizedError.ts
+++ src/framework/errors/UnauthorizedError.ts
@@ -1,12 +1,12 @@
-import { HttpStatusCode } from '../enums/HttpStatusCode'
-import { LambdaError } from './LambdaError'
+import { LambdaError, HttpStatusCode } from '../../aws-lambda-framework'
 
 export class UnauthorizedError extends LambdaError {
   constructor(
     errorMessage: string = 'Unauthorized User. Ensure that user is in the correct cognito user group.',
+    stackTrace?: string,
     userMessage: string = 'User is not authorized!',
     statusCode: number = HttpStatusCode.Unauthorized
   ) {
-    super(errorMessage, userMessage, statusCode)
+    super(errorMessage, stackTrace, userMessage, statusCode)
   }
 }
--- src/framework/errors/ValidationError.ts
+++ src/framework/errors/ValidationError.ts
@@ -1,15 +1,15 @@
-import { HttpStatusCode } from '../enums/HttpStatusCode'
-import { LambdaError } from './LambdaError'
+import { LambdaError, HttpStatusCode } from '../../aws-lambda-framework'
 
 export class ValidationError extends LambdaError {
   validationErrors: any[]
   constructor(
     validationErrors: any[],
     errorMessage: string = 'Invalid input. Check validationErrors property for more information.',
+    stackTrace?: string,
     userMessage: string = 'Invalid input!',
     statusCode: number = HttpStatusCode.BadRequest
   ) {
-    super(errorMessage, userMessage, statusCode)
+    super(errorMessage, stackTrace, userMessage, statusCode)
     this.validationErrors = validationErrors
   }
 }
