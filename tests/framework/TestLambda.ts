a72e729596a77d1cd0f8f9c31d994b7e0cdbe548
Dan Olsen <do@intelligentbanker.com>
2020-01-08 17:29:49 +0100

    Refactor: put validateInput code into function for testing validation

--- tests/framework/TestLambda.ts
+++ tests/framework/TestLambda.ts
@@ -1,39 +1,22 @@
-import { TestInput } from './constants/TestInput'
 import {
   BaseLambda,
   LambdaContainer,
-  InputValidator,
   Mysql,
-  Property,
   LambdaError,
   APIGatewayProxyEvent,
   Context,
-  APIGatewayProxyResult,
-  ValidationError
+  APIGatewayProxyResult
 } from '../../src/aws-lambda-framework'
 
 class TestLambda extends BaseLambda {
-  input: TestInput
-
   constructor(event: APIGatewayProxyEvent, context: Context) {
     super(event, context)
-    const request = LambdaContainer.get<TestInput>(Property.EVENT_BODY)
-    this.input = new TestInput(request.testString, request.testNumber)
   }
 
   async invoke(): Promise<any> {
-    await this.validateInput()
     const res = await LambdaContainer.get(Mysql).execute({ sql: process.env.MYSQL_TEST_QUERY!, inputs: [1] })
     if (!res.success) throw new LambdaError(res.error.message, res.error.stack)
     return res.result
-  }
-
-  private async validateInput() {
-    return LambdaContainer.get(InputValidator)
-      .validateOrReject(this.input)
-      .catch(errors => {
-        throw new ValidationError(errors)
-      })
   }
 }
 
