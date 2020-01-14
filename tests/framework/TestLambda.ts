bd576d7b3b0cab34fc9939f4a16a1542da759f1e
Dan Olsen <do@intelligentbanker.com>
2020-01-14 10:35:37 +0100

    Fix: new result syntax

--- tests/framework/TestLambda.ts
+++ tests/framework/TestLambda.ts
@@ -8,15 +8,22 @@ import {
   APIGatewayProxyResult
 } from '../../src/aws-lambda-framework'
 
+interface Country {
+  id: number
+  name: string
+  locale: string
+  countryCode: number
+}
+
 class TestLambda extends BaseLambda {
   constructor(event: APIGatewayProxyEvent, context: Context) {
     super(event, context)
   }
 
   async invoke(): Promise<any> {
-    const res = await LambdaContainer.get(Mysql).execute({ sql: process.env.MYSQL_TEST_QUERY!, inputs: [1] })
+    const res = await LambdaContainer.get(Mysql).execute<Country>({ sql: 'select * from countries' })
     if (!res.success) throw new LambdaError(res.error.message, res.error.stack)
-    return res.result
+    return res.result.rows
   }
 }
 
