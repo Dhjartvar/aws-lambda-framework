d9b317f356c12cc8bcc9ad648b88c480801f938b
Dhjartvar <do@intelligentbanker.com>
2020-01-22 19:18:19 +0100

    Refactor: Removed unit tests for AWS services as these should be handled by AWS themselves

--- tests/framework/services/Lambda.test.ts
+++
@@ -1,7 +0,0 @@
-import { LambdaContainer, Lambda } from '../../../src/aws-lambda-framework'
-
-describe('Lambda', () => {
-  it('should resolve when trying to list functions', async () => {
-    await expect(LambdaContainer.get(Lambda).listFunctions().promise()).resolves.toBeDefined()
-  })
-})
--- tests/framework/services/S3.test.ts
+++
@@ -1,7 +0,0 @@
-import { LambdaContainer, S3 } from '../../../src/aws-lambda-framework'
-
-describe('S3', () => {
-  it('should resolve when trying to list buckets ', async () => {
-    await expect(LambdaContainer.get(S3).listBuckets().promise()).resolves.toBeDefined()
-  })
-})
