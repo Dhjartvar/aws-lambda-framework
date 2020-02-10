865a1f6a0d10a48bc2cc071cb9e73403130e107d
Dan Olsen <do@intelligentbanker.com>
2020-02-10 15:42:57 +0100

    Refactor: Readded SlackNotificationColor enum for color input

--- src/framework/interfaces/Notifier.ts
+++ src/framework/interfaces/Notifier.ts
@@ -1,3 +1,5 @@
+import { SlackNotificationColor } from 'lambda-slack-notifier/dist/SlackNotificationColor'
+
 export default interface Notifier {
-  notify(message: any, notificationColor?: string): Promise<any>
+  notify(message: any, notificationColor?: string | SlackNotificationColor): Promise<any>
 }
