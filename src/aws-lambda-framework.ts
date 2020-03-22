3d06b72185dd5d1bce6604a1eba6a0f9807b55cc
Dan Olsen <dhjartvar@pop-os.localdomain>
2020-03-22 23:43:20 +0100

    Fix: exports from aws-sdk

--- src/aws-lambda-framework.ts
+++ src/aws-lambda-framework.ts
@@ -12,36 +12,12 @@ export {
   APIGatewayProxyResult,
   DynamoDBStreamEvent,
   CloudWatchLogsEvent,
-  SNSEvent,
+  SNSMessage,
   SQSEvent
 } from 'aws-lambda'
 export { CognitoToken } from './framework/interfaces/CognitoToken'
 export { Query } from './framework/interfaces/Query'
-import {
-  S3 as s3,
-  Lambda as lambda,
-  SSM as ssm,
-  DynamoDB as ddb,
-  RDS as rds,
-  Redshift as rs,
-  SES as ses,
-  Kinesis as kinesis,
-  APIGateway as apig,
-  CloudWatch as cw,
-  CloudFront as cf
-} from 'aws-sdk'
-export const S3 = s3
-export const Lambda = lambda
-export const SSM = ssm
-export const DynamoDB = ddb
-export const DynamoDC = ddb.DocumentClient
-export const RDS = rds
-export const Redshift = rs
-export const SES = ses
-export const Kinesis = kinesis
-export const APIGateway = apig
-export const CloudWatch = cw
-export const CloudFront = cf
+export { S3, Lambda, SSM, DynamoDB, RDS, Redshift, Kinesis, APIGateway, CloudWatch, CloudFront, SES } from 'aws-sdk'
 export { APIGatewayLambda } from './framework/APIGatewayLambda'
 export { CloudWatchLambda } from './framework/CloudWatchLambda'
 export { DynamoDBStreamsLambda } from './framework/DynamoDBStreamsLambda'
